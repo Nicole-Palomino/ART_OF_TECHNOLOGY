29970445cefcc332dfae30bb46abf979da89bc05
Nicole-Palomino <alessa.kz.fx@gmail.com>
2023-07-09 22:26:57 -0500

    interfaz de menu principal y de registro

--- app/src/main/java/zegel/ipae/proyectofinal/view/menuAdmin/MenuAdminActivity.kt
+++ app/src/main/java/zegel/ipae/proyectofinal/view/menuAdmin/MenuAdminActivity.kt
@@ -6,6 +6,9 @@ import zegel.ipae.proyectofinal.R
 
 class MenuAdminActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Ocultar el Action Bar
+        supportActionBar?.hide()
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_admin)
     }
--- app/src/main/java/zegel/ipae/proyectofinal/view/register/RegisterActivity.kt
+++ app/src/main/java/zegel/ipae/proyectofinal/view/register/RegisterActivity.kt
@@ -1,12 +1,25 @@
 package zegel.ipae.proyectofinal.view.register
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import zegel.ipae.proyectofinal.R
+import zegel.ipae.proyectofinal.view.login.LoginActivity
+import zegel.ipae.proyectofinal.view.menuAdmin.MenuAdminActivity
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Ocultar el Action Bar
+        supportActionBar?.hide()
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val text1 = findViewById<TextView>(R.id.txtSession)
+
+        text1.setOnClickListener {
+            startActivity(Intent(this@RegisterActivity, LoginActivity::class.java))
+        }
     }
 }
