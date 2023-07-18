3cb4d6109791908e7a5de6912b4a4fc15a211bf9
Nicole Palomino <alessa.kz.fx@gmail.com>
2023-07-18 15:18:46 -0500

    Creacion de la interfaz gestion de comprobante

--- app/src/main/java/zegel/ipae/proyectofinal/view/gestionProductos/GestionProductosActivity.kt
+++ app/src/main/java/zegel/ipae/proyectofinal/view/gestionProductos/GestionProductosActivity.kt
@@ -1,12 +1,26 @@
 package zegel.ipae.proyectofinal.view.gestionProductos
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import zegel.ipae.proyectofinal.R
+import zegel.ipae.proyectofinal.view.menuAdmin.MenuAdminActivity
 
 class GestionProductosActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gestion_productos)
+
+        // aparezca el icono de regresar
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            startActivity(Intent(this, MenuAdminActivity::class.java))
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/zegel/ipae/proyectofinal/view/menuAdmin/MenuAdminActivity.kt
+++ app/src/main/java/zegel/ipae/proyectofinal/view/menuAdmin/MenuAdminActivity.kt
@@ -1,8 +1,12 @@
 package zegel.ipae.proyectofinal.view.menuAdmin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.cardview.widget.CardView
 import zegel.ipae.proyectofinal.R
+import zegel.ipae.proyectofinal.view.comprobante.ComprobanteActivity
 
 class MenuAdminActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +15,11 @@ class MenuAdminActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_admin)
+
+        val button = findViewById<CardView>(R.id.cardSales)
+
+        button.setOnClickListener {
+            startActivity(Intent(this@MenuAdminActivity, ComprobanteActivity::class.java))
+        }
     }
 }
